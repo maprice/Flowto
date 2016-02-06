8ce17df3e861e49a5059a0302c9f3002be1fd772
Mike Price <mprice@zynga.com>
2016-02-05 22:10:51 -0800

    Added photo model and parsed JSON

--- MPFlowto/app/src/main/java/com/example/mprice/mpflowto/MainActivity.java
+++ MPFlowto/app/src/main/java/com/example/mprice/mpflowto/MainActivity.java
@@ -1,13 +1,76 @@
 package com.example.mprice.mpflowto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CLIENT_ID = "e05c462ebd86446ea48a5af73769b602";
+    private ArrayList<PhotoModel> photos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        photos = new ArrayList<>();
+
+
+        makeNetworkRequest();
+
+    }
+
+    private void makeNetworkRequest() {
+        String URL = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+
+                JSONArray photosJSON = null;
+
+                try {
+                    photosJSON = response.getJSONArray("data");
+
+                    for (int i = 0 ; i < photosJSON.length(); i++) {
+                        JSONObject photoJSON = photosJSON.getJSONObject(i);
+
+                        PhotoModel photo = new PhotoModel();
+
+                        photo.userName = photoJSON.getJSONObject("user").getString("userrname");
+                        photo.imageCaption = photoJSON.getJSONObject("caption").getString("text");
+                        photo.imageURL = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                        photo.likes = photoJSON.getJSONObject("likes").getInt("count");
+
+                        photos.add(photo);
+                    }
+                } catch (JSONException e) {
+
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+
+            }
+        });
+
+
     }
 }
---
+++ MPFlowto/app/src/main/java/com/example/mprice/mpflowto/PhotoModel.java
@@ -0,0 +1,15 @@
+package com.example.mprice.mpflowto;
+
+/**
+ * Created by mprice on 2/5/16.
+ */
+public class PhotoModel {
+
+    String userName;
+    String imageCaption;
+    String imageURL;
+    int imageHeight;
+    int likes;
+
+
+}
